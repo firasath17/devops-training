53c90e0ab8e071c0e22a8f84dd24e4e323cd2a72
root <root@ip-172-31-54-32.ec2.internal>
2021-01-12 03:27:57 +0000

    maven build 1

---
+++ sfm-app/src/test/java/com/engg/trp/AppTest.java
@@ -0,0 +1,20 @@
+package com.engg.trp;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
